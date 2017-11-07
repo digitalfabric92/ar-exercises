b11a33ef478e5a7193d7d544b9d12a5e7913db53
digitalfabric92 <dwawryko@gmail.com>
2017-11-07 12:42:35 -0500

    Exercise 5 Complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,8 +7,9 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
-
-puts Store.sum(:annual_revenue)}
-# revenue/store
-# .where ('annual_revnue' > 100000).count
+# 1 Person.sum(:age) from stack overflow
+puts "TOTAL REVENUE: ", Store.sum(:annual_revenue)
+# 2 revenue/store
+puts "AVERAGE REVENUE: ", Store.sum(:annual_revenue) / Store.count
+# 3 .where ('annual_revnue' > 100000).count
+puts "$1 MIL or More: ", Store.where("annual_revenue > ?", 1000000).count
